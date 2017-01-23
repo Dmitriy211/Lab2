eecab9623b126f4a6d8a5c496dacb2803dbb0aa6
Dmitriy211 <dmitriy.211@mail.ru>
2017-01-23 16:59:42 +0600

    Complex

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Complex
+    {
+        int a, b;
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c3 = new Complex(0,0);
+            c3.a = c1.a + c2.a;
+            c3.b = c1.b + c2.b;
+            return c3;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}+{1}i", a, b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(5, 6);
+            Complex c2 = new Complex(8, 1);
+
+            Console.WriteLine(c1 + c2);
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class student
+    {
+        string name;
+        string surname;
+        string full;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                full = name + ' ' + surname;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+                full = name + ' ' + surname;
+            }
+        }
+        public string Full { get { return full; } }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            s.Name = "d";
+            s.Surname = "t";
+           
+            Console.WriteLine(s.Full);
+        }
+    }
+}
